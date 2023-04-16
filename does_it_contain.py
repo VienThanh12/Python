b4341bdf428c5fa92540f105dc2a5cec97c5bfab
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-16 20:51:09 +0300

    Create does_it_contain.py
    Signed-off-by: Kevin <67015555+VienThanh12@users.noreply.github.com>

---
+++ does_it_contain.py
@@ -0,0 +1,14 @@
+s = input("Please type in a string: ")
+if 'a' in s:
+    print("a found")
+else: 
+    print("a not found")
+if 'e' in s:
+    print("e found")
+else: 
+    print("e not found")
+
+if 'o' in s:
+    print("o found")
+else: 
+    print("o not found")
